5bdc1a64f3a4489ffc73232035bc1a2035f6bc13
Yonom <sepehr1376@gmail.com>
2014-02-23 18:22:46 +0100

    Add tests for constructors of MuffinClient

--- MuffinFramework.Tests/MuffinClientTests.cs
+++ MuffinFramework.Tests/MuffinClientTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MuffinFramework.Tests
@@ -29,6 +31,77 @@ namespace MuffinFramework.Tests
 
             // assert
             Assert.IsNotNull(expectedException);
+        }
+
+        [TestMethod]
+        public void Constructor1Test()
+        {
+            // arrange
+
+            // act
+            var client = new MuffinClient();
+
+            // assert
+            Assert.IsNotNull(client.MuffinLoader);
+            Assert.IsNotNull(client.ServiceLoader);
+            Assert.IsNotNull(client.PlatformLoader);
+
+            Assert.IsNotNull(client.AggregateCatalog);
+            Assert.AreEqual(1, client.AggregateCatalog.Catalogs.Count);
+        }
+
+
+        [TestMethod]
+        public void Constructor2Test()
+        {
+            // arrange
+            var catalog = new AggregateCatalog();
+
+            // act
+            var client = new MuffinClient(catalog);
+
+            // assert
+            Assert.IsNotNull(client.MuffinLoader);
+            Assert.IsNotNull(client.ServiceLoader);
+            Assert.IsNotNull(client.PlatformLoader);
+            Assert.AreSame(catalog, client.AggregateCatalog);
+        }
+
+
+        [TestMethod]
+        public void Constructor3Test()
+        {
+            // arrange
+            var catalog1 = new TypeCatalog();
+            var catalog2 = new TypeCatalog();
+
+            // act
+            var client = new MuffinClient(catalog1, catalog2);
+
+            // assert
+            Assert.IsNotNull(client.MuffinLoader);
+            Assert.IsNotNull(client.ServiceLoader);
+            Assert.IsNotNull(client.PlatformLoader);
+
+            Assert.IsNotNull(client.AggregateCatalog);
+            Assert.AreEqual(2, client.AggregateCatalog.Catalogs.Count);
+        }
+
+        [TestMethod]
+        public void Constructor4Test()
+        {
+            // arrange
+
+            // act
+            var client = new MuffinClient(new ComposablePartCatalog[] {});
+
+            // assert
+            Assert.IsNotNull(client.MuffinLoader);
+            Assert.IsNotNull(client.ServiceLoader);
+            Assert.IsNotNull(client.PlatformLoader);
+
+            Assert.IsNotNull(client.AggregateCatalog);
+            Assert.AreEqual(0, client.AggregateCatalog.Catalogs.Count);
         }
     }
 }
